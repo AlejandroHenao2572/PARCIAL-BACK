5fc71c4a87e0a1b411df7caa9e826ac59042877a
unknown <david.patacon-h@labinfo.is.escuelaing.edu.co>
2026-02-24 16:14:28 -0500

    initial config

---
+++ src/main/java/com/parcial_back/ParcialBackApplication.java
@@ -0,0 +1,13 @@
+package com.parcial_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialBackApplication.class, args);
+	}
+
+}
